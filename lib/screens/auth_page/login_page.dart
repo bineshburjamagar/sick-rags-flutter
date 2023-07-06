2a2c6146a900469e4ecc3249da70a86b78770afd
burzza <bineshburzaa@gmail.com>
2023-07-06 13:57:48 +0545

    signup page animations

--- lib/screens/auth_page/login_page.dart
+++ lib/screens/auth_page/login_page.dart
@@ -5,7 +5,7 @@ import 'package:sick_rags_flutter/config/config.dart';
 import 'package:sick_rags_flutter/screens/auth_page/signup_page.dart';
 import 'package:sick_rags_flutter/screens/nav_page/home_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
   static const String routeName = "/loginpage";
   static Route route() {
@@ -15,29 +15,10 @@ class LoginPage extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: const Scaffold(
-        backgroundColor: AppColors.primaryColor,
-        body: SingleChildScrollView(
-          child: WelcomeBackWidget(),
-        ),
-      ),
-    );
-  }
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class WelcomeBackWidget extends StatefulWidget {
-  const WelcomeBackWidget({
-    super.key,
-  });
-
-  @override
-  State<WelcomeBackWidget> createState() => _WelcomeBackWidgetState();
-}
-
-class _WelcomeBackWidgetState extends State<WelcomeBackWidget> {
+class _LoginPageState extends State<LoginPage> {
   double margin = 380;
 
   @override
@@ -46,104 +27,120 @@ class _WelcomeBackWidgetState extends State<WelcomeBackWidget> {
     changeToOriginal();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(seconds: 1),
-      margin: EdgeInsets.only(top: margin),
-      child: Column(
-        children: [
-          const Text(
-            'SICK RAGS',
-            style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Caveat',
-                fontWeight: FontWeight.w700,
-                color: Colors.white),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(30.0),
-              ),
-            ),
-            padding:
-                const EdgeInsets.symmetric(horizontal: 23.0, vertical: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'Welcome Back !',
-                  style: TextStyle(
-                      fontFamily: 'Caveat',
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w600),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Don\'t have an account? ',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () =>
-                          Navigator.pushNamed(context, SignUpPage.routeName),
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(
-                          color: Colors.indigo,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 40.0),
-                const CustomTextField(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  isRequired: true,
-                ),
-                const SizedBox(height: 20.0),
-                const CustomTextField(
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  isPassword: true,
-                  isRequired: true,
-                ),
-                const SizedBox(height: 80.0),
-                CustomButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(
-                        HomePage.routeName,
-                      );
-                    },
-                    label: 'Sign In')
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   void changeToOriginal() {
     Future.delayed(const Duration(milliseconds: 10), () {
       setState(() {
         margin = 100;
       });
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: AppColors.primaryColor,
+        body: SingleChildScrollView(
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            margin: EdgeInsets.only(top: margin),
+            child: Column(
+              children: [
+                const Text(
+                  'SICK RAGS',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'Caveat',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 23.0, vertical: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Welcome Back !',
+                        style: TextStyle(
+                            fontFamily: 'Caveat',
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account? ',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () => Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder:
+                                    (context, animation1, animation2) =>
+                                        const SignUpPage(),
+                                transitionDuration: Duration.zero,
+                                reverseTransitionDuration: Duration.zero,
+                              ),
+                            ),
+                            child: const Text(
+                              'Register',
+                              style: TextStyle(
+                                color: Colors.indigo,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 40.0),
+                      const CustomTextField(
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                        isRequired: true,
+                      ),
+                      const SizedBox(height: 20.0),
+                      const CustomTextField(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        isPassword: true,
+                        isRequired: true,
+                      ),
+                      const SizedBox(height: 80.0),
+                      CustomButton(
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(
+                              HomePage.routeName,
+                            );
+                          },
+                          label: 'Sign In')
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/auth_page/signup_page.dart
+++ lib/screens/auth_page/signup_page.dart
@@ -3,7 +3,7 @@ import 'package:sick_rags_flutter/components/custom_button.dart';
 import 'package:sick_rags_flutter/components/custom_text_field.dart';
 import 'package:sick_rags_flutter/config/config.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
   static const String routeName = "/signuppage";
   static Route route() {
@@ -13,103 +13,121 @@ class SignUpPage extends StatelessWidget {
   }
 
   @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  double margin = 100;
+
+  @override
+  void initState() {
+    super.initState();
+    changeToOriginal();
+  }
+
+  void changeToOriginal() {
+    Future.delayed(const Duration(milliseconds: 10), () {
+      setState(() {
+        margin = 50;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         backgroundColor: AppColors.primaryColor,
         body: SingleChildScrollView(
-          child: Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                color: Colors.black,
-                padding: const EdgeInsets.only(
-                  top: 80,
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            margin: EdgeInsets.only(top: margin),
+            child: Column(
+              children: [
+                const Text(
+                  'SICK RAGS',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'Caveat',
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
                 ),
-                child: const Center(
-                  child: Text(
-                    'SICK RAGS',
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        fontFamily: 'Caveat',
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white),
-                  ),
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 180),
-                height: MediaQuery.of(context).size.height,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                  ),
-                ),
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 23.0, vertical: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Create an account.',
-                      style: TextStyle(
-                          fontFamily: 'Caveat',
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.w600),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Already a member? ',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () => Navigator.pop(context),
-                          child: const Text(
-                            'Sign In',
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 23.0, vertical: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Create an account.',
+                        style: TextStyle(
+                            fontFamily: 'Caveat',
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Already a member? ',
                             style: TextStyle(
-                              color: Colors.indigo,
                               fontSize: 16,
                               fontWeight: FontWeight.w500,
                             ),
                           ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 40.0),
-                    const CustomTextField(
-                      labelText: 'Email',
-                      hintText: 'Enter your email',
-                      isRequired: true,
-                    ),
-                    const SizedBox(height: 20.0),
-                    const CustomTextField(
-                      labelText: 'Password',
-                      hintText: 'Enter your password',
-                      isPassword: true,
-                      isRequired: true,
-                    ),
-                    const SizedBox(height: 20.0),
-                    const CustomTextField(
-                      labelText: 'Confirm Password',
-                      hintText: 'Re-enter your password',
-                      isPassword: true,
-                      isRequired: true,
-                    ),
-                    const SizedBox(height: 80.0),
-                    CustomButton(onPressed: () {}, label: 'Sign Up')
-                  ],
-                ),
-              )
-            ],
+                          InkWell(
+                            onTap: () => Navigator.pop(context),
+                            child: const Text(
+                              'Sign In',
+                              style: TextStyle(
+                                color: Colors.indigo,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 40.0),
+                      const CustomTextField(
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                        isRequired: true,
+                      ),
+                      const SizedBox(height: 20.0),
+                      const CustomTextField(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        isPassword: true,
+                        isRequired: true,
+                      ),
+                      const SizedBox(height: 20.0),
+                      const CustomTextField(
+                        labelText: 'Confirm Password',
+                        hintText: 'Re-enter your password',
+                        isPassword: true,
+                        isRequired: true,
+                      ),
+                      const SizedBox(height: 80.0),
+                      CustomButton(onPressed: () {}, label: 'Sign Up')
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
